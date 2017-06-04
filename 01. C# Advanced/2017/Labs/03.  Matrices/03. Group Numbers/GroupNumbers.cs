e4f4070253da8b5e68182a96479df0bbc8150fe5
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-06-04 19:12:56 +0300

    More presentations and homework. Solution of Matrices lab

---
+++ 01. C# Advanced/2017/Labs/03.  Matrices/03. Group Numbers/GroupNumbers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace _03.Group_Numbers
+{
+    class GroupNumbers
+    {
+        static void Main()
+        {
+            //First way
+            int[] numbers = Console.ReadLine().Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] sizes = new int[3];
+
+            foreach (var number in numbers)
+            {
+                int reminder = Math.Abs(number % 3);
+                sizes[reminder]++;
+            }
+
+            int[][] matrix = new int[3][]
+            {
+                new int[sizes[0]],
+                new int[sizes[1]],
+                new int[sizes[2]]
+            };
+
+            int[] offsets = new int[3];
+            foreach (var number in numbers)
+            {
+                int remider = Math.Abs(number % 3);
+                int index = offsets[remider];
+                offsets[remider]++;
+                matrix[remider][index] = number;
+            }
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+
+            //Second way
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+            //var numbers = Console.ReadLine().Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            //var zero = numbers.Where(n => Math.Abs(n) % 3 == 0);
+            //var one = numbers.Where(n => Math.Abs(n) % 3 == 1);
+            //var two = numbers.Where(n => Math.Abs(n) % 3 == 2);
+
+            //watch.Stop();
+            //Console.WriteLine(watch.ElapsedTicks);
+
+            //Console.WriteLine(string.Join(" ", zero));
+            //Console.WriteLine(string.Join(" ", one));
+            //Console.WriteLine(string.Join(" ", two));
+        }
+    }
+}
---
+++ 01. C# Advanced/2017/Labs/03.  Matrices/04. Pascal Triangle/PascalTriangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04.Pascal_Triangle
+{
+    class PascalTriangle
+    {
+        static void Main()
+        {
+            //Second Solution !Dosent work with big numbers!
+            //var maxnumber = long.Parse(Console.ReadLine());
+            //for (int i = 0; i < maxnumber; i++)
+            //{
+            //    long startNumber = 1;
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write(startNumber);
+            //        Console.Write(" ");
+            //        startNumber = startNumber * (i - j) / (j + 1);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //First solution
+            ////var height = int.Parse(Console.ReadLine());
+
+            ////long[][] pascal =  new long[height][];
+
+            ////for (int row = 0; row < height; row++)
+            ////{
+            ////    pascal[row] = new long[row + 1];
+            ////    pascal[row][0] = 1;
+            ////    pascal[row][pascal[row].Length - 1] = 1;
+
+            ////    for (int col = 1; col < pascal[row].Length - 1; col++)
+            ////    {
+            ////        pascal[row][col] = pascal[row - 1][col - 1] + pascal[row - 1][col];
+            ////    }
+            ////}
+            ////foreach (var row in pascal)
+            ////{
+            ////    Console.WriteLine(string.Join(" ", row));
+            ////}
+        }
+    }
+}
