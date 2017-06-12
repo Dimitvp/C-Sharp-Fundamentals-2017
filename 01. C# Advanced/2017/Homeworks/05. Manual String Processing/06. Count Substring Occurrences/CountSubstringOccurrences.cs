19951103e42b7862d61d53efb698c82d78daf996
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-06-12 23:16:35 +0300

    Solving some problems for homework

---
+++ 01. C# Advanced/2017/Homeworks/05. Manual String Processing/06. Count Substring Occurrences/CountSubstringOccurrences.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.Count_Substring_Occurrences
+{
+    class CountSubstringOccurrences
+    {
+        static void Main()
+        {
+            var text = Console.ReadLine();
+            var pattern = Console.ReadLine();
+            var counter = 0;
+
+            //First way
+            foreach (var word in text)
+            {
+                if (b)
+                {
+                    counter++;
+                }
+            }
+
+            //Second way
+            //int maxIndex = text.Length - pattern.Length;
+            //for (int i = 0; i <= maxIndex; i++)
+            //{
+            //    string substring = text.Substring(i, pattern.Length);
+            //    if (substring.Equals(pattern))
+            //    {
+            //        counter++;
+            //    }
+            //}
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
---
+++ 01. C# Advanced/2017/Homeworks/05. Manual String Processing/08. Multiply big number/MultiplyBigNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Multiply_big_number
+{
+    class MultiplyBigNumber
+    {
+        static void Main()
+        {
+            var firstNum = new Stack<char>(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            var result = new StringBuilder();
+            var sum = 0;
+            var reminder = 0;
+            var multiplyer = 0;
+            //TODO everything is wrong
+            while (firstNum.Count != 0)
+            {
+                sum = multiplyer / 10 + reminder;
+                reminder = 0;
+                multiplyer = secondNum * int.Parse(firstNum.Pop().ToString());
+                if (multiplyer + sum > 9)
+                {
+                    reminder = ((multiplyer % 10) + sum) / 10;
+                    
+                    result.Insert(0, reminder);
+                }
+                else
+                {
+                    result.Insert(0, (multiplyer % 10) + sum);
+                }
+            }
+
+            result.Insert(0, multiplyer / 10);
+            Console.WriteLine(result.ToString().TrimStart('0'));
+        }
+    }
+}
